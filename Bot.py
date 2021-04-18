fa6fe17ee77c76041536e226cfc055704d30388c
Dannoopsy <63581325+Dannoopsy@users.noreply.github.com>
2021-04-18 11:51:21 +0300

    Update Bot.py
    add downloading images

--- Bot.py
+++ Bot.py
@@ -1,9 +1,13 @@
 import telebot
+import os
 
 TOKEN = '1715580463:AAGSuz7c8EKO43wt_0w6-Yfct8vcOfkVO6U'
 name = ''
 num = 0
 bot = telebot.TeleBot(TOKEN)
+
+#def do_nth () :
+
 
 def reg_name(message):
     global name
@@ -26,6 +30,29 @@ def send_welcome(message):
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(message, "Эль псай конгру")
+
+@bot.message_handler(content_types=['voice'])
+def handler(message):
+    bot.send_message(message.chat.id, 'Напиши словами, не могу послушать')
+
+
+@bot.message_handler(content_types=['photo'])
+def photo_handler(message):
+    if os.path.exists('images_from_users/{' + str(message.from_user.id) + '}'):
+        directory = 'images_from_users/{' + str(message.from_user.id) + '}'
+    else:
+        os.mkdir('images_from_users/{' + str(message.from_user.id) + '}')
+        directory = 'images_from_users/{' + str(message.from_user.id) + '}'
+    photo = message.photo[-1]
+    file_id = photo.file_id
+    file_path = bot.get_file(file_id).file_path
+    downloaded_file = bot.download_file(file_path)
+    name = file_id + ".jpg"
+    new_file = open(directory + '/' + name, mode='wb')
+    new_file.write(downloaded_file)
+    new_file.close()
+    bot.send_message(message.from_user.id, 'Принял photo_handler')
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
